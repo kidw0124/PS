17b15a21ca8d30333a44293fc7f08ba5b6c1d8f8
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-02 02:04:48 +0900

    [Diamond IV] Title: 경비병 세우기 게임, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Diamond/18939. 경비병 세우기 게임/경비병 세우기 게임.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem=1ull<<63-1){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j;
+    ll t=1;
+
+    cin>>t;
+
+    while(t--){
+        ll n,m,k;
+        cin>>n>>m>>k;
+        if(m<2*k||n*m%2){
+            cout<<"Yuto\n";
+        }
+        else{
+            cout<<"Platina\n";
+        }
+    }
+
+    return 0;
+}
