892185ae819c373352f266682d4f871c1e0c5158
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-02 00:27:02 +0900

    [Platinum I] Title: 수학 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/2862. 수학 게임/수학 게임.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+ll n;
+vl fibo;
+int main(){
+//    ios_base::sync_with_stdio(0);
+//    cin.tie(0);
+    ll i,j,ans=0;
+    cin>>n;
+    fibo.push_back(1);
+    fibo.push_back(2);
+    for(;*fibo.rbegin()<n;){
+        fibo.push_back(*fibo.rbegin()+*(fibo.rbegin()+1));
+    }
+    if(n==*fibo.rbegin())cout<<n;
+    else{
+        ll t=n-*(fibo.rbegin()+1);
+        while(t){
+            t-=ans=*(upper_bound(fibo.begin(),fibo.end(),t)-1);
+        }
+        cout<<ans;
+    }
+
+    return 0;
+}
