777a04256f15c98651eb41e205527df02af86242
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-11 14:57:15 +0900

    [Bronze IV] Title: 욱 제, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Bronze/17356. 욱 제/욱 제.cc
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,j;
+    long double a,b;
+    cin>>a>>b;
+    cout<<fixed<<setprecision(5)<<1/(1+pow(10,(b-a)/400));
+    return 0;
+}
