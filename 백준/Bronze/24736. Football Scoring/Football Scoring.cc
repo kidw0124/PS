359fbb684d94ca9b5a4cd4a55a0d287561e6071b
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-16 15:36:10 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a*6+b*3+c*2+d+e*2<<' ';
+    cin>>a>>b>>c>>d>>e;
+    cout<<a*6+b*3+c*2+d+e*2<<' ';
+}
