248b108226c1c275d7910e1ca0d1f3f03a908867
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-07-25 15:15:24 +0900

    [Gold V] Title: 루트 게임, Time: 48 ms, Memory: 2264 KB -BaekjoonHub

---
+++ 백준/Gold/16888. 루트 게임/루트 게임.cc
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+typedef long long ll;
+typedef long long LL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (!ndebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifdef ONLINE_JUDGE
+constexpr bool ndebug = true;
+#else
+constexpr bool ndebug = false;
+#endif
+
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	ll i,j;
+	ll t,n;
+	cin>>t;
+	vector<bool>arr(1e6+1);
+	for(i=0;i<=1e6;i++){
+		if(arr[i])continue;
+		else{
+			for(j=1;i+j*j<=1e6;j++){
+				arr[i+j*j]=true;
+			}
+		}
+	}
+	while(t--){
+		ll n;
+		cin>>n;
+		cout<<vector<string>({"koosaga","cubelover"})[!arr[n]]<<'\n';
+	}
+	return 0;
+}
