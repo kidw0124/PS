d6dd199ba0383b92a5640ad5444aa29ac57d1495
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-09-20 20:30:04 +0900

    [Silver IV] Title: 수 찾기, Time: 68 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int arr[100001];
+void Solution(int n, int key){
+    int start = 0;
+    int end = n-1;
+    int mid;
+    while(end - start >= 0){
+        mid = (start + end)/2;
+        if(arr[mid] == key){
+            printf("1\n");
+            return ;
+        }else if(arr[mid] > key) { //key 값이 배열의 중앙 값보다 작을때 (왼쪽으로)
+            end = mid - 1;
+
+        }else{  //key 값이 배열의 중앙 값보다 클때 (오른쪽으로)
+            start = mid + 1;
+        }
+    }
+
+    printf("0\n");
+    return ;
+}
+
+int main(void){
+
+    int n, m, tmp;
+
+    //입력
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    sort(arr, arr+n);   //quick sort를 이용해 배열 오름차순으로 정렬
+
+    //입력
+    scanf("%d", &m);
+
+    for(int i=0; i<m; i++){
+        scanf("%d", &tmp);
+        Solution(n, tmp);
+    }
+
+    return 0;
+}
