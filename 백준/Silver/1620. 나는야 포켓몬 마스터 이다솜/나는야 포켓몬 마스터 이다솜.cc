fb5954775510de5b666a9a3f6243bed8f37f38bb
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-09-20 20:40:05 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 124 ms, Memory: 19980 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,40 +1,34 @@
 #include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<set>
+#include<string.h>
 using namespace std;
-typedef long long ll;
-typedef long long LL;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vl;
-#define pb(x) push_back(x)
-#define all(x) (x).begin(), (x).end()
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
-ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
-ll POW(ll a, ll b, ll rem=1ull<<63-1){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll i,j;
-    ll n,m;
-    cin>>n>>m;
-    map<string,int>mp;
-    vector<string>arr(n+1);
-    for(i=1;i<=n;i++){
-        cin>>arr[i];
-        mp[arr[i]]=i;
+    cout.tie(0);
+    unordered_map<string,int>mp;
+    vector<string> arr;
+    int n;
+    int q;
+    cin>>n>>q;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        arr.push_back(s);
+        mp[s]=i+1;
     }
-    for(i=0;i<m;i++){
-        string str;
-        cin>>str;
-        if(isalpha(str[0])){
-            cout<<mp[str]<<'\n';
+    for(int i=0;i<q;i++){
+        string s;
+        cin>>s;
+        if(isdigit(s[0])){
+            cout<<arr[stoi(s)-1]<<'\n';
         }
         else{
-            cout<<arr[stoll(str)]<<'\n';
+            cout<<mp[s]<<'\n';
         }
     }
-
-    return 0;
 }
