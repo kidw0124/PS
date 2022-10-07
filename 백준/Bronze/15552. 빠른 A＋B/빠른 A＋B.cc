eed5b366594c80ef43167fc0fa8c423224d6b5e3
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-10-08 06:28:51 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 #pragma warning(disable:4996)
 #pragma comment(linker, "/STACK:336777216")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("avx2")
+#include <immintrin.h>
 using namespace std;
 typedef long long ll;
 typedef long long LL;
@@ -28,6 +31,9 @@ ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
 ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
 ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
 
+alignas(16) short A[16]{ 1, 2, 3, 1, 2, 3, 1, 2 };
+alignas(16) short B[16]{ 1, 2, 3, 4, 5, 6, 7, 8 };
+alignas(16) short C[16];
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
@@ -35,10 +41,24 @@ int main(){
 	ll i,j;
 	ll t;
 	cin>>t;
-	for(ll i=0;i<t;i++){
-		ll a,b;
-		cin>>a>>b;
-		cout<<a+b<<' ';
+	int td16=t/16,tm16=t%16;
+	for(int i=0;i<td16;i++){
+		for(int j=0;j<16;j++){
+			cin>>A[j]>>B[j];
+		}
+		__m256i a = _mm256_load_si256((__m256i*)A);
+		__m256i b = _mm256_load_si256((__m256i*)B);
+		__m256i c = _mm256_add_epi16(a, b);
+		_mm256_store_si256((__m256i*)C, c);
+		for (int j = 0; j < 16; j++) cout << C[j] << '\n';
 	}
+	for(int i=0;i<tm16;i++){
+		cin>>A[i]>>B[i];
+	}
+	__m256i a = _mm256_load_si256((__m256i*)A);
+	__m256i b = _mm256_load_si256((__m256i*)B);
+	__m256i c = _mm256_add_epi16(a, b);
+	_mm256_store_si256((__m256i*)C, c);
+	for (int j = 0; j < tm16; j++) cout << C[j] << '\n';
 	return 0;
 }
