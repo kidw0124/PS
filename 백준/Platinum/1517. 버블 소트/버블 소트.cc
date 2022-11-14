50b7846c8f74626a733b3a2545a4421570bc40a7
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-11-14 19:51:00 +0900

    [Platinum V] Title: 버블 소트, Time: 200 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.cc
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -1,59 +1,88 @@
 #include<bits/stdc++.h>
+using namespace std;
 #pragma warning(disable:4996)
 #pragma comment(linker, "/STACK:336777216")
-using namespace std;
 typedef long long ll;
-typedef long long LL;
+typedef unsigned long long ull;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 typedef pair<ll, ll> pll;
 typedef vector<ll> vl;
 #define pb(x) push_back(x)
 #define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (xdebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (xdebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (xdebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+ostream& operator<<(ostream&os,pll x){
+    os<<"{"<<x.first<<","<<x.second<<"}";
+}
+
+#ifndef ONLINE_JUDGE
+constexpr bool xdebug = true;
+#else
+constexpr bool xdebug = false;
+#endif
 ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
 ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
-ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+ll powm(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)%rem)if(b&1)p=(p*a)%rem;return p;}
 
-deque<ll> arr;
-deque<ll> le, ri;
-ll n,ans;
-void merge_sort(deque<ll>::iterator it,ll st, ll en){
-    if(st<en){
-        merge_sort(it,st,(st+en)>>1);
-        merge_sort(it,((st+en)>>1)+1,en);
-        deque<ll> tmp;
-        ll i=st,j=1+((st+en)>>1);
-        while(i<=((st+en)>>1)&&j<=en){
-            if(it[i]<=it[j]){
-                tmp.push_back(it[i++]);
-            }
-            else{
-                ans+=((st+en)>>1)-i+1;
-                tmp.push_back(it[j++]);
-            }
+void mmerge(vector<ll>&arr, ll l, ll mid, ll r, ll&cnt){
+    vector<ll>tmp;
+    ll x=l,y=mid;
+    while(x<mid&&y<r){
+        if(arr[x]<=arr[y]){
+            tmp.push_back(arr[x++]);
         }
-        while(i<=((st+en)>>1)){
-            tmp.push_back(it[i++]);
+        else{
+            cnt+=mid-x;
+            tmp.push_back(arr[y++]);
         }
-        while(j<=en){
-            tmp.push_back(it[j++]);
-        }
-        for(i=st,j=0;i<=en;i++,j++)it[i]=tmp[j];
     }
+    while(y<r){
+        tmp.push_back(arr[y++]);
+    }
+    while(x<mid){
+        tmp.push_back(arr[x++]);
+    }
+    copy(tmp.begin(),tmp.end(),arr.begin()+l);
+}
+
+void msort(vector<ll>&arr, ll l, ll r, ll&cnt){
+    if(l+1>=r){
+        return;
+    }
+    else{
+        ll mid=(l+r)>>1;
+        msort(arr,l,mid,cnt);
+        msort(arr,mid,r,cnt);
+        mmerge(arr,l,mid,r,cnt);
+    }
+}
+
+ll merge_sort(vector<ll>arr){
+    ll cnt=0;
+    msort(arr,0,arr.size(),cnt);
+    return cnt;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     ll i,j;
+    ll n;
     cin>>n;
+    vector<ll>arr(n);
     for(i=0;i<n;i++){
-        ll t;
-        cin>>t;
-        arr.pb(t);
+        cin>>arr[i];
     }
-    merge_sort(arr.begin(),0,n-1);
-    cout<<ans;
+    cout<<merge_sort(arr);
 
     return 0;
 }
