fafcbe20f3132ef6bd8b04cf014eab21b152b32b
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2022-12-22 00:08:08 +0900

    [Bronze IV] Title: 시험 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=0,b=0,t;
+    cin>>t;
+    a+=t;
+    cin>>t;
+    a+=t;
+    cin>>t;
+    a+=t;
+    cin>>t;
+    a+=t;
+    cin>>t;
+    b+=t;
+    cin>>t;
+    b+=t;
+    cin>>t;
+    b+=t;
+    cin>>t;
+    b+=t;
+    cout<<max(a,b);
+}
