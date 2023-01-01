430d1160a4e31620931d413d7c17a38bb6e5d441
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2023-01-02 02:45:03 +0900

    [Gold IV] Title: 시로코와 은행털기, Time: 352 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.cc
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (xdebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (xdebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (xdebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifndef ONLINE_JUDGE
+constexpr bool xdebug = true;
+#else
+constexpr bool xdebug = false;
+#endif
+void setup(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll powm(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)%rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main(){
+    setup();
+    ll i,j;
+    ll n,k,x;
+    cin>>n>>k>>x;
+    vector<pll>arr(n);
+    for(i=0;i<n;i++){
+        cin>>arr[i].first>>arr[i].second;
+    }
+    vector<set<ll>>brr(vector<set<ll>>(k+1));
+    for(i=0;i<n;i++){
+        for(j=k-1;j>0;j--){
+            for(auto t:brr[j]){
+                brr[j+1].insert(t+arr[i].first);
+            }
+        }
+        brr[1].insert(arr[i].first);
+    }
+    ll ans=0;
+    for(auto t:brr[k]){
+        ans=max(ans,t*(k*x-t));
+    }
+    cout<<ans;
+}
