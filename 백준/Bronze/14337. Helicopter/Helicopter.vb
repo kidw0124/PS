4aab62ffee0e498c94f4ec7ed3a0edf8d7edfefc
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2023-01-14 01:07:26 +0900

    [Bronze V] Title: Helicopter, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,16 @@
+'                             Online VB Compiler.
+'                 Code, Compile, Run and Debug VB program online.
+' Write your code in this editor and press "Run" button to execute it.
+
+
+Module VBModule
+    Sub Main()
+        Console.WriteLine("      ===================")
+        Console.WriteLine("          ____||___")
+        Console.WriteLine("\ /      /       []\")
+        Console.WriteLine(" X=======           \__")
+        Console.WriteLine("/ \      \____________|")
+        Console.WriteLine("            ||  ||")
+        Console.WriteLine("         \-----------/")
+    End Sub
+End Module
