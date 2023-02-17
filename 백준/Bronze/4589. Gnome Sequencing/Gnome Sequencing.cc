7da3da268ac56c14be1135a49290b33a4ca51dc7
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2023-02-17 17:21:36 +0900

    [Bronze IV] Title: Gnome Sequencing, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4589. Gnome Sequencing/Gnome Sequencing.cc
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+typedef long long ll;
+typedef unsigned long long ull;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i,a,b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto& x: a)
+#define endl '\n'
+
+#define debug if constexpr (xdebug) cout << "[DEBUG] "
+#define debugv(x) if constexpr (xdebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
+#define debugc(c) if constexpr (xdebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
+
+#ifndef ONLINE_JUDGE
+constexpr bool xdebug = true;
+#else
+constexpr bool xdebug = false;
+#endif
+void setup(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+}
+ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
+ll powm(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)%rem)if(b&1)p=(p*a)%rem;return p;}
+
+int main() {
+    setup();
+    ll i,j,n;
+    cin>>n;
+    cout<<"Gnomes:\n";
+    while(n--){
+        vector<ll>arr(3),brr,crr;
+        for(auto &k:arr)cin>>k;
+        brr=arr;
+        std::sort(arr.begin(), arr.end());
+        crr=arr;
+        std::reverse(crr.begin(), crr.end());
+        if(arr==brr||brr==crr){
+            cout<<"Ordered\n";
+        }
+        else cout<<"Unordered\n";
+    }
+}
