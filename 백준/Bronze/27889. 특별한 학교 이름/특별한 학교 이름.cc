2a812d2983945f831cac62e54462825d003b9f48
김동우 <38641848+kidw0124@users.noreply.github.com>
2023-04-06 21:36:36 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    if(str== "NLCS")
+        cout<<"North London Collegiate School";
+    if(str== "BHA")
+        cout<<"Branksome Hall Asia";
+    if(str== "KIS")
+        cout<<"Korea International School";
+    if(str== "SJA")
+        cout<<"St. Johnsbury Academy";
+}
