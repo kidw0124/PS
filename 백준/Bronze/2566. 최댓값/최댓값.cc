92997f2dda7eca6600ab13644845d7b1532fc963
김동우 <38641848+kidw0124@users.noreply.github.com>
2023-04-11 20:07:00 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -1,39 +1,22 @@
-#include<bits/stdc++.h>
-#pragma warning(disable:4996)
-#pragma comment(linker, "/STACK:336777216")
-using namespace std;
-typedef long long ll;
-typedef long long LL;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vl;
-#define pb(x) push_back(x)
-#define all(x) (x).begin(), (x).end()
-#define rep(i,a,b) for (auto i = (a); i < (b); i++)
-#define each(x, a) for (auto& x: a)
-#define endl '\n'
-
-#define debug if constexpr (!ndebug) cout << "[DEBUG] "
-#define debugv(x) if constexpr (!ndebug) cout << "[DEBUG] " << #x << " == " << x << '\n';
-#define debugc(c) if constexpr (!ndebug) { cout << "[DEBUG] "<< #c << ": "; for (const auto& elem : c) cout << elem << ", "; cout << '\n'; }
-
-#ifdef ONLINE_JUDGE
-constexpr bool ndebug = true;
-#else
-constexpr bool ndebug = false;
-#endif
-
-ll gcd(ll a, ll b){return b?gcd(b,a%b):a;}
-ll lcm(ll a, ll b){if(a&&b)return a*(b/gcd(a,b)); return a+b;}
-ll POW(ll a, ll b, ll rem){ll p=1;for(;b;b/=2,a=(a*a)% rem)if(b&1)p=(p*a)%rem;return p;}
+#include<stdio.h>
 
 int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	ll i,j;
-	vector<ll>arr(81);
-	each(i,arr)cin>>i;
-	cout<<*max_element(all(arr))<<'\n'<<(max_element(all(arr))-arr.begin())/9+1<<' '<<(max_element(all(arr))-arr.begin())%9+1;
-	return 0;
+    int i,j;
+    int arr[9][9];
+    for(i=0;i<9;i++){
+        for(j=0;j<9;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int max=arr[0][0],x=0,y=0;
+    for(i=0;i<9;i++){
+        for(j=0;j<9;j++){
+            if(arr[i][j]>max){
+                max=arr[i][j];
+                x=i;
+                y=j;
+            }
+        }
+    }
+    printf("%d %d %d",max,x+1,y+1);
 }
