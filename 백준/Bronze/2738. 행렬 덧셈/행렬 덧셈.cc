ad69d40e4d14bdadb1810d263c325ea0d5a2a570
김동우 <38641848+kidw0124@users.noreply.github.com>
2023-04-11 20:11:41 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -1,78 +1,29 @@
-#ifndef __MATRIX_H__
-#define __MATRIX_H__
-
-#include <vector>
-using namespace std;
-
-class MatrixInt{
-private:
-    vector<vector<int>> matrix;
-public:
-    // Constructors
-    MatrixInt(int rows, int columns);
-    MatrixInt(const MatrixInt& other);
-    MatrixInt(MatrixInt&& other);
-    MatrixInt();
-
-    void add(const MatrixInt& other);
-    void input();
-    void print();
-
-};
-#ifndef __MATRIX_CPP__
-#define __MATRIX_CPP__
-
-#include <iostream>
-
-
-MatrixInt::MatrixInt(int rows, int columns) : matrix(rows, vector<int>(columns)) {}
-MatrixInt::MatrixInt(const MatrixInt& other) : matrix(other.matrix) {}
-MatrixInt::MatrixInt(MatrixInt&& other) : matrix(move(other.matrix)) {}
-MatrixInt::MatrixInt() : matrix(0, vector<int>(0)) {}
-
-void MatrixInt::add(const MatrixInt& other) {
-    if (matrix.size() != other.matrix.size() || matrix[0].size() != other.matrix[0].size()) {
-        throw "Matrix sizes do not match";
-    }
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix[0].size(); j++) {
-            matrix[i][j] += other.matrix[i][j];
-        }
-    }
-}
-
-void MatrixInt::input() {
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix[0].size(); j++) {
-            cin >> matrix[i][j];
-        }
-    }
-}
-
-void MatrixInt::print() {
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix[0].size(); j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << '\n';
-    }
-}
-
-
-#endif // __MATRIX_CPP__
-#endif
-#include <iostream>
-typedef long long int ll;
+#include<stdio.h>
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ll n, m;
-    cin >> n >> m;
-    MatrixInt mat(n, m), mat2(n, m);
-    mat.input();
-    mat2.input();
-    mat.add(mat2);
-    mat.print();
+    int i,j;
+    int arr[105][105],brr[105][105];
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&brr[i][j]);
+        }
+    }
+    int crr[105][105];
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            crr[i][j]=arr[i][j]+brr[i][j];
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ",crr[i][j]);
+        }
+    }
 }
