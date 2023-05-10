49ffc2c96c06df641a592a0521fc98b790d8fd5f
김동우 <38641848+kidw0124@users.noreply.github.com>
2023-05-10 20:51:18 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,33 +1,40 @@
-#include<bits/stdc++.h>
 #include<iostream>
 #include<string>
 #include<algorithm>
-#include<set>
-#include<string.h>
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    unordered_set<int> a;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin>>n;
+    int arr[100'000+5];
     for(int i=0;i<n;i++){
-        int x;
-        cin>>x;
-        a.insert(x);
+        cin>>arr[i];
     }
+    sort(arr,arr+n);
     int m;
     cin>>m;
     for(int i=0;i<m;i++){
+        int st=0,en=n-1;
         int x;
         cin>>x;
-        if(a.find(x)!=a.end()){
-            cout<<"1\n";
+        while(st<=en){
+            int mid = (st+en)/2;
+            if(arr[mid]<x){
+                st=mid+1;
+            }
+            else if(arr[mid]>x){
+                en=mid-1;
+            }
+            else{
+                cout<<1<<'\n';
+                break;
+            }
         }
-        else{
-            cout<<"0\n";
+        if(st>en){
+            cout<<0<<'\n';
         }
     }
+    
 }
