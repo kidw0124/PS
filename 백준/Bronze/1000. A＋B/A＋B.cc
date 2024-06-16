8b3fdb22f9df52e750aab040ee5089e37f48821f
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-06-16 17:37:01 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,8 +1,10 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
     int a,b;
+    ///scanf("%d %d",&a,&b);
     cin>>a>>b;
+    ///printf("%d",a+b);
     cout<<a+b;
 }
