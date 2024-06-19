cd41af3bb95c601f7b0c9e85756aa34459fd3367
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-06-20 03:44:23 +0900

    [Ruby IV] Title: 제곱 부분문자열, Time: 296 ms, Memory: 19864 KB -BaekjoonHub

---
+++ 백준/Ruby/18441. 제곱 부분문자열/제곱 부분문자열.cc
@@ -0,0 +1,476 @@
+#define private public
+#include<bitset>
+#undef private
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pll = pair<ll,ll>;
+using ld = long double;
+using pld = pair<ld,ld>;
+typedef unsigned long long ull;
+typedef __float128 LD;
+typedef __int128_t LL;
+typedef __uint128_t ULL;
+typedef pair<ll, ll> pii;
+typedef vector<ll> vi;
+typedef pair<ll, ll> pll;
+typedef vector<ll> vl;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace __gnu_pbds;
+#pragma warning(disable : 4996)
+#pragma comment(linker, "/STACK:336777216")
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#define pb(x) push_back(x)
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (auto i = (a); i < (b); i++)
+#define each(x, a) for (auto &x : a)
+#define endl '\n'
+#ifdef kidw0124
+constexpr bool ddebug = true;
+#else
+constexpr bool ddebug = false;
+#endif
+#define debug if constexpr(ddebug) cout << "[DEBUG] "
+#include <x86intrin.h>
+
+template<size_t _Nw> void _M_do_sub(_Base_bitset<_Nw> &A, const _Base_bitset<_Nw> &B) {
+    for(int i=0, c=0; i<_Nw; i++)
+#ifdef BOJ
+        c=_subborrow_u64(c, A._M_w[i], B._M_w[i], (unsigned long long*)&A._M_w[i]);
+#else
+        c=_subborrow_u32(c, A._M_w[i], B._M_w[i], (unsigned int*)&A._M_w[i]);
+#endif
+}
+template<> void _M_do_sub(_Base_bitset<1> &A, const _Base_bitset<1> &B) {
+    A._M_w-=B._M_w;
+}
+template<size_t _Nb> bitset<_Nb>& operator-=(bitset<_Nb> &A, const bitset<_Nb> &B) {
+    _M_do_sub(A, B);
+    return A;
+}
+template<size_t _Nb> inline bitset<_Nb> operator-(const bitset<_Nb> &A, const bitset<_Nb> &B) {
+    bitset<_Nb> C(A);
+    return C-=B;
+}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b)
+{
+    if (a && b)
+        return a * (b / gcd(a, b));
+    return a + b;
+}
+ll powm(ll a, ll b, ll rem)
+{
+    ll p = 1;
+    for (; b; b /= 2, a = (a * a) % rem)
+        if (b & 1)
+            p = (p * a) % rem;
+    return p;
+}
+pair<ll, ll> extended_gcd(ll a, ll b)
+{
+    if (b == 0)
+        return {1, 0};
+    auto t = extended_gcd(b, a % b);
+    return {t.second, t.first - t.second * (a / b)};
+}
+ll modinverse(ll a, ll m)
+{
+    return (extended_gcd(a, m).first % m + m) % m;
+}
+
+LD eps = 1e-12;
+ll large_mod_mul(ll a, ll b, ll m)
+{
+    return ll((__int128)a * (__int128)b % m);
+}
+ll modpow(ll n, ll k, ll m)
+{
+    ll ret = 1;
+    n %= m;
+    while (k)
+    {
+        if (k & 1)
+            ret = large_mod_mul(ret, n, m);
+        n = large_mod_mul(n, n, m);
+        k /= 2;
+    }
+    return ret;
+}
+bool test_witness(ull a, ull n, ull s)
+{
+    if (a >= n)
+        a %= n;
+    if (a <= 1)
+        return true;
+    ull d = n >> s;
+    ull x = modpow(a, d, n);
+    if (x == 1 || x == n - 1)
+        return true;
+    while (s-- > 1)
+    {
+        x = large_mod_mul(x, x, n);
+        if (x == 1)
+            return false;
+        if (x == n - 1)
+            return true;
+    }
+    return false;
+}
+bool is_prime(ull n)
+{
+    if (n == 2)
+        return true;
+    if (n < 2 || n % 2 == 0)
+        return false;
+    ull d = n >> 1, s = 1;
+    for (; (d & 1) == 0; s++)
+        d >>= 1;
+#define T(a) test_witness(a##ull, n, s)
+    if (n < 4759123141ull)
+        return T(2) && T(7) && T(61);
+    return T(2) && T(325) && T(9375) && T(28178) && T(450775) && T(9780504) && T(1795265022);
+#undef T
+}
+ll pollard_rho(ll n)
+{
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<ll> dis(1, n - 1);
+    ll x = dis(gen);
+    ll y = x;
+    ll c = dis(gen);
+    ll g = 1;
+    while (g == 1)
+    {
+        x = (large_mod_mul(x, x, n) + c) % n;
+        y = (large_mod_mul(y, y, n) + c) % n;
+        y = (large_mod_mul(y, y, n) + c) % n;
+        g = gcd(abs(x - y), n);
+    }
+    return g;
+}
+void factorize(ll n, unordered_map<ll, ll> &fl)
+{
+    if (n == 1)
+    {
+        return;
+    }
+    if (n % 2 == 0)
+    {
+        fl[2]++;
+        factorize(n / 2, fl);
+    }
+    else if (is_prime(n))
+    {
+        fl[n]++;
+    }
+    else
+    {
+        ll f = pollard_rho(n);
+        factorize(f, fl);
+        factorize(n / f, fl);
+    }
+}
+
+#ifndef kidw0124
+const ll TSIZE = 1 << 20; // always 2^k form && n <= TSIZE
+#else
+const ll TSIZE = 1 << 3; // always 2^k form && n <= TSIZE
+#endif
+
+struct segment
+{
+    vector<ll> segtree, prop, dat;
+    segment(ll n)
+    {
+        segtree.resize(TSIZE * 2);
+        prop.resize(TSIZE * 2);
+        dat.resize(n);
+    }
+    void seg_init(ll nod, ll l, ll r)
+    {
+        if (l == r)
+            segtree[nod] = dat[l];
+        else
+        {
+            ll m = (l + r) >> 1;
+            seg_init(nod << 1, l, m);
+            seg_init(nod << 1 | 1, m + 1, r);
+            segtree[nod] = segtree[nod << 1] + segtree[nod << 1 | 1];
+        }
+    }
+
+    void seg_relax(ll nod, ll l, ll r)
+    {
+        if (prop[nod] == 0)
+            return;
+        if (l < r)
+        {
+            ll m = (l + r) >> 1;
+            segtree[nod << 1] += (m - l + 1) * prop[nod];
+            prop[nod << 1] += prop[nod];
+            segtree[nod << 1 | 1] += (r - m) * prop[nod];
+            prop[nod << 1 | 1] += prop[nod];
+        }
+        prop[nod] = 0;
+    }
+
+    ll seg_query(ll nod, ll l, ll r, ll s, ll e)
+    {
+        if (r < s || e < l)
+            return 0;
+        if (s <= l && r <= e)
+            return segtree[nod];
+        seg_relax(nod, l, r);
+        ll m = (l + r) >> 1;
+        return seg_query(nod << 1, l, m, s, e) + seg_query(nod << 1 | 1, m + 1, r, s, e);
+    }
+
+    void seg_update(ll nod, ll l, ll r, ll s, ll e, ll val)
+    {
+        if (r < s || e < l)
+            return;
+        if (s <= l && r <= e)
+        {
+            segtree[nod] += (r - l + 1) * val;
+            prop[nod] += val;
+            return;
+        }
+        seg_relax(nod, l, r);
+        ll m = (l + r) >> 1;
+        seg_update(nod << 1, l, m, s, e, val);
+        seg_update(nod << 1 | 1, m + 1, r, s, e, val);
+        segtree[nod] = segtree[nod << 1] + segtree[nod << 1 | 1];
+    }
+    // usage:
+    // seg_update(1, 0, n - 1, qs, qe, val);
+    // seg_query(1, 0, n - 1, qs, qe);
+};
+
+const ll mod = 998244353;
+const int alpha_num=26;
+vector<bitset<3005>>S_3000(alpha_num,bitset<3005>());
+vector<bitset<2005>>S_2000(alpha_num,bitset<2005>());
+vector<bitset<1005>>S_1000(alpha_num,bitset<1005>());
+vector<bitset<505>>S_500(alpha_num,bitset<505>());
+vector<bitset<255>>S_250(alpha_num,bitset<255>());
+
+ll get_lcs_3000(const char* a, const char* b, int n, int m){
+    ll i,j;
+    bitset<3005>D,x;
+    for(i=0;i<m;i++){
+        S_3000[b[i]-'a'][i]=1;
+    }
+    for(i=0;i<n;i++){
+        x=D|S_3000[a[i]-'a'];
+        D<<=1;
+        D|=1;
+        D=x&(x^(x-D));
+    }
+    for(i=0;i<m;i++){
+        S_3000[b[i]-'a'][i]=0;
+    }
+    return D.count();
+}
+
+ll get_lcs_2000(const char* a, const char* b, int n, int m){
+    ll i,j;
+    bitset<2005>D,x;
+    for(i=0;i<m;i++){
+        S_2000[b[i]-'a'][i]=1;
+    }
+    for(i=0;i<n;i++){
+        x=D|S_2000[a[i]-'a'];
+        D<<=1;
+        D|=1;
+        D=x&(x^(x-D));
+    }
+    for(i=0;i<m;i++){
+        S_2000[b[i]-'a'][i]=0;
+    }
+    return D.count();
+}
+
+ll get_lcs_1000(const char* a, const char* b, int n, int m){
+    ll i,j;
+    bitset<1005>D,x;
+    for(i=0;i<m;i++){
+        S_1000[b[i]-'a'][i]=1;
+    }
+    for(i=0;i<n;i++){
+        x=D|S_1000[a[i]-'a'];
+        D<<=1;
+        D|=1;
+        D=x&(x^(x-D));
+    }
+    for(i=0;i<m;i++){
+        S_1000[b[i]-'a'][i]=0;
+    }
+    return D.count();
+}
+
+ll get_lcs_500(const char* a, const char* b, int n, int m){
+    ll i,j;
+    bitset<505>D,x;
+    for(i=0;i<m;i++){
+        S_500[b[i]-'a'][i]=1;
+    }
+    for(i=0;i<n;i++){
+        x=D|S_500[a[i]-'a'];
+        D<<=1;
+        D|=1;
+        D=x&(x^(x-D));
+    }
+    for(i=0;i<m;i++){
+        S_500[b[i]-'a'][i]=0;
+    }
+    return D.count();
+}
+
+ll get_lcs_250(const char* a, const char* b, int n, int m){
+    ll i,j;
+    bitset<255>D,x;
+    for(i=0;i<m;i++){
+        S_250[b[i]-'a'][i]=1;
+    }
+    for(i=0;i<n;i++){
+        x=D|S_250[a[i]-'a'];
+        D<<=1;
+        D|=1;
+        D=x&(x^(x-D));
+    }
+    for(i=0;i<m;i++){
+        S_250[b[i]-'a'][i]=0;
+    }
+    return D.count();
+}
+
+string get_lcs_string(string a, string b){
+    ll i,j;
+    vector<vector<ll>>D(a.size()+1,vector<ll>(b.size()+1));
+    for(i=1;i<=a.size();i++){
+        for(j=1;j<=b.size();j++){
+            if(a[i-1]==b[j-1]){
+                D[i][j]=D[i-1][j-1]+1;
+            }
+            else{
+                D[i][j]=max(D[i-1][j],D[i][j-1]);
+            }
+        }
+    }
+    string ans;
+    i=a.size();
+    j=b.size();
+    while(i>0&&j>0){
+        if(a[i-1]==b[j-1]){
+            ans+=a[i-1];
+            i--;
+            j--;
+        }
+        else if(D[i-1][j]>D[i][j-1]){
+            i--;
+        }
+        else{
+            j--;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+void solve(ll testcase){
+    int i, j, k;
+    cout<<"Case #"<<testcase<<": ";
+    string str;
+    cin>>str;
+    ll n=str.size();
+    ll ans=0;
+    ll now=0;
+    ll t;
+    for(i=0;i<n;i++){
+        ll no=min((ll)i,n-i);
+        if(no>=2000) {
+            if(i<n-i){
+                t=get_lcs_3000(str.data()+i,str.data(),n-i,i);
+            }
+            else{
+                t=get_lcs_3000(str.data(),str.data()+i,i,n-i);
+            }
+        }
+        else if(no>=1000) {
+            if(i<n-i){
+                t=get_lcs_2000(str.data()+i,str.data(),n-i,i);
+            }
+            else{
+                t=get_lcs_2000(str.data(),str.data()+i,i,n-i);
+            }
+        }
+        else if(no>=500) {
+            if(i<n-i){
+                t=get_lcs_1000(str.data()+i,str.data(),n-i,i);
+            }
+            else{
+                t=get_lcs_1000(str.data(),str.data()+i,i,n-i);
+            }
+        }
+        else if(no>=250) {
+            if(i<n-i){
+                t=get_lcs_500(str.data()+i,str.data(),n-i,i);
+            }
+            else{
+                t=get_lcs_500(str.data(),str.data()+i,i,n-i);
+            }
+        }
+        else {
+            if(i<n-i){
+                t=get_lcs_250(str.data()+i,str.data(),n-i,i);
+            }
+            else{
+                t=get_lcs_250(str.data(),str.data()+i,i,n-i);
+            }
+        }
+        debug<<i<<" "<<t<<endl;
+        if(t>ans){
+            ans=t;
+            now=i;
+        }
+    }
+    cout<<ans*2<<endl;
+    if(ans){
+        string cc = get_lcs_string(str.substr(0,now),str.substr(now,n-now));
+        cout<<cc<<cc<<endl;
+    }
+}
+
+void setup() {
+    if(ddebug) {
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    }
+    else {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+}
+
+void preprocess() {
+    ll i, j, k;
+
+}
+
+int main() {
+    setup();
+    preprocess();
+    ll t = 1;
+    cin >> t;
+    for (ll testcase = 1; testcase <= t; testcase++)
+    {
+        solve(testcase);
+    }
+    return 0;
+}
