ce197767bca7b25a6fa87b38167c22d1b9c64690
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-07-08 00:56:52 +0900

    [Platinum V] Title: Asking for Money, Time: 64 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Platinum/28040. Asking for Money/Asking for Money.cc
+++ 백준/Platinum/28040. Asking for Money/Asking for Money.cc
@@ -1,33 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef pair<ll, ll> pll;
 
 void solve() {
-    ll i,j,k;
-    ll n;
+    int i,j;
+    int n;
     cin>>n;
-    vector<pll>grp(n);
-    vector<unordered_set<ll>>rgrp(n);
+    vector<pair<int,int>>grp(n+1);
+    vector<vector<int>>rgrp(n+1);
     string ans;
-    for(i=0;i<n;i++){
+    for(i=1;i<=n;i++){
         cin>>grp[i].first>>grp[i].second;
-        grp[i].first--,grp[i].second--;
-        rgrp[grp[i].first].insert(i);
-        rgrp[grp[i].second].insert(i);
-        ans+='N';
+        rgrp[grp[i].first].push_back(i);
+        rgrp[grp[i].second].push_back(i);
     }
-    function<vector<bool>(ll,ll)> dfs=[&rgrp,&n](ll s, ll e)->vector<bool>{
-        stack<ll>stk;
+    function<vector<bool>(int,int)> dfs=
+            [&rgrp,&n](int s, int inter)->vector<bool>{
+        stack<int>stk;
         stk.push(s);
-        bool ret=0;
-        vector<bool>vis(n);
+        vector<bool>vis(n+1);
         vis[s]=1;
         while(stk.size()){
-            ll now=stk.top();
+            int now=stk.top();
             stk.pop();
             for(auto k:rgrp[now]){
-                if(vis[k]||k==e)continue;
+                if(vis[k]||k==inter)continue;
                 else{
                     vis[k]=1;
                     stk.push(k);
@@ -36,19 +32,23 @@ void solve() {
         }
         return vis;
     };
-    for(i=0;i<n;i++){
+    for(i=1;i<=n;i++){
         auto vis1=dfs(grp[i].first,i);
         auto vis2=dfs(grp[i].second,i);
         auto vis3=dfs(i,-1);
-        for(j=0;j<n;j++){
-            if(vis1[j]&&vis2[j]&&vis3[j])ans[i]='Y';
+        for(j=1;j<=n;j++){
+            if(vis1[j]&&vis2[j]&&vis3[j]){
+                ans+='Y';
+                break;
+            }
         }
+        if(j==n+1)ans+='N';
     }
     cout<<ans;
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
+#ifdef kidw0124
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #else
