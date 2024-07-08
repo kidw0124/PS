3494242322c98cd8377ef949cc2d6409197a326c
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-07-09 06:48:54 +0900

    [Platinum V] Title: 정수론 싫어, Time: 20 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Platinum/4924. 정수론 싫어/정수론 싫어.cc
+++ 백준/Platinum/4924. 정수론 싫어/정수론 싫어.cc
@@ -1,49 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
-struct segtree{
-    struct Node{
-        ll lprifix, rprefix, sum, maxsum;
-    };
-    vector<ll>dat;
-    vector<Node>seg;
-    segtree(){
-        dat.resize(1<<20);
-        seg.resize(1<<21);
-    }
-    void seg_init(int nod, int l, int r){
-        if(l==r){
-            seg[nod]={dat[l], dat[l], dat[l], dat[l]};
-            return;
-        }
-        int mid=(l+r)>>1;
-        seg_init(nod<<1, l, mid);
-        seg_init(nod<<1|1, mid+1, r);
-        seg[nod].sum=seg[nod<<1].sum+seg[nod<<1|1].sum;
-        seg[nod].lprifix=max(seg[nod<<1].lprifix, seg[nod<<1].sum+seg[nod<<1|1].lprifix);
-        seg[nod].rprefix=max(seg[nod<<1|1].rprefix, seg[nod<<1|1].sum+seg[nod<<1].rprefix);
-        seg[nod].maxsum=max({seg[nod].lprifix, seg[nod].rprefix, seg[nod<<1].maxsum, seg[nod<<1|1].maxsum, seg[nod<<1].rprefix+seg[nod<<1|1].lprifix});
-    }
-    Node seg_query(int nod, int l, int r, int s, int e){
-        if(e<l||r<s) return {INT_MIN, INT_MIN, INT_MIN, INT_MIN};
-        if(s<=l&&r<=e) return seg[nod];
-        int mid=(l+r)>>1;
-        Node L=seg_query(nod<<1, l, mid, s, e);
-        Node R=seg_query(nod<<1|1, mid+1, r, s, e);
-        Node ret;
-        ret.sum=L.sum+R.sum;
-        ret.lprifix=max(L.lprifix, L.sum+R.lprifix);
-        ret.rprefix=max(R.rprefix, R.sum+L.rprefix);
-        ret.maxsum=max({ret.lprifix, ret.rprefix, L.maxsum, R.maxsum, L.rprefix+R.lprifix});
-        return ret;
-    }
-};
-
 void solve() {
-    ll i,j,cnt=0;
-    vector<ll> spf(1'000'000+1);
+    int i,j,cnt=0;
+    vector<int> spf(1'000'000+1);
     for(i=2;i<=1'000'000;++i) {
         if(spf[i]==0) {
             for(j=i;j<=1'000'000;j+=i) {
@@ -51,7 +11,7 @@ void solve() {
             }
         }
     }
-    vector<ll> primes(1e6+1);
+    vector<int> primes(1e6+1);
     primes[1]=0;
     for(i=2;i<=1'000'000;++i) {
         primes[i]=primes[i/spf[i]]+1;
@@ -60,11 +20,14 @@ void solve() {
         if(primes[i]==1)primes[i]=-2;
         else primes[i]--;
     }
-    segtree seg;
-    seg.dat=primes;
-    seg.seg_init(1, 1, 1'000'000);
     while(cin>>i>>j, i!=-1&&j!=-1) {
-        cout<< ++cnt <<". " << seg.seg_query(1, 1, 1'000'000, i, j).maxsum << '\n';
+        cout<< ++cnt <<". ";
+        int ans=primes[i],now=0;
+        for(int k=i;k<=j;++k) {
+            now=max(0,now)+primes[k];
+            ans=max(ans,now);
+        }
+        cout<<ans<<'\n';
     }
 }
 
