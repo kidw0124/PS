355e586c76fc893815242f716631013a3b568d2f
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-07-19 05:06:35 +0900

    [Gold IV] Title: 알파벳, Time: 1776 ms, Memory: 216712 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = int;
+using pll = pair<ll,ll>;
+using tlll = tuple<ll,ll,ll>;
+int main(){
+    ll i,j,k;
+    ll n,m;
+    cin>>n>>m;
+    vector<string>str(n);
+    for(i=0;i<n;i++)cin>>str[i];
+    ll ans=0;
+    queue<pair<pll,ll>>q;
+    q.push({{0,0},1<<(str[0][0]-'a')});
+    while(!q.empty()) {
+        auto [pos, mask] = q.front();
+        q.pop();
+        auto [x, y] = pos;
+        ans = max(ans, (ll)__builtin_popcount(mask));
+        for (auto [dx, dy]: vector<pll>{{1,  0},
+                                        {0,  1},
+                                        {-1, 0},
+                                        {0,  -1}}) {
+            ll nx = x + dx, ny = y + dy;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m)continue;
+            if (mask & (1 << (str[nx][ny] - 'a')))continue;
+            ll nmask = mask | (1 << (str[nx][ny] - 'a'));
+            q.push({{nx, ny}, nmask});
+        }
+    }
+    cout<<ans<<endl;
+
+}
