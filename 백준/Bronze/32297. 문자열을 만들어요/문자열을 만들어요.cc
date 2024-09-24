82417fc635b6db0c98754c91d2265bf8fa129e01
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-09-24 23:39:08 +0900

    [Bronze III] Title: 문자열을 만들어요, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32297. 문자열을 만들어요/문자열을 만들어요.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string str;
+    cin>>str;
+    if(str.find("gori")!=string::npos)cout<<"YES";
+    else cout<<"NO";
+}
