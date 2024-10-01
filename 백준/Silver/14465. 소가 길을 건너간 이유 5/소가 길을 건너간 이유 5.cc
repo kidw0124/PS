d26082a2b607c085018bf1cb5a38ae257ffa4cb6
김동우 <38641848+kidw0124@users.noreply.github.com>
2024-10-01 18:07:58 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 0 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
@@ -5,39 +5,33 @@ using ll = long long;
 void solve() {
     ll i,j;
     /// N개의 횡단보도가 있다
-    /// arr[i] : i번 신호등이 있으면 1, 없으면 0
-    /// 구하고 싶은 것
-    /// arr[i],arr[i+1],...,arr[i+k-1] 모두 1이 되게 하기 위해
-    /// 필요한 최소한의 신호등의 개수
-    /// 이 중 이미 1인것이 가장 많은 구간
+    /// arr[i] : i번이 고장났으면 1, 고장안났으면 0
     /// arr[i]+arr[i+1]+...+arr[i+k-1]
-    /// => 이미 있는 신호등의 개수
-    /// 0, 1배열에서 "합=1의 개수"
-    /// sum[i+k-1]-sum[i-1]의 최대
-    /// i가 가능한 것 : n-k+1개 : O(N)
+    /// i~i+k-1까지 고장난것의 개수
+    /// sum[0]=0;
+    /// sum[1]=arr[0];
+    /// sum[2]=arr[0]+arr[1];
+    /// sum[i] = arr[0]+arr[1]+...+arr[i-1]
+    /// sum[i+k]-sum[i] : i~i+k-1까지 고장난것의 개수
+    /// 이것의 최솟값
     ll n,k,b;
     cin>>n>>k>>b;
-    vector<ll>arr(n,1),sum(n+1);
+    vector<ll>arr(n),sum(n+1);
     for(i=0;i<b;i++){
         ll x;
         cin>>x;
-        arr[x-1]=0;
+        arr[x-1]=1;
     }
     // sum[0]=0; => 선언할때 초기화가 이미 됨
     for(i=1;i<=n;i++){
         sum[i]=sum[i-1]+arr[i-1];
     }
-    ll ans=0;
-    // i+k-1 <= n
-    for(i=1;i+k-1<=n;i++){
-        ll tmp=sum[i+k-1]-sum[i-1];
-        if(ans<tmp){
-            ans=tmp;
-        }
+    ll ans=k;
+    // i+k <= n
+    for(i=0;i+k<=n;i++){
+        ans=min(ans,sum[i+k]-sum[i]);
     }
-    cout<<k-ans;
-
-
+    cout<<ans;
 }
 
 int main() {
